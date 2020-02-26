b63164a24b1c5dcbd0e545fdd5ab0133482b5004
Kevin Minutti <minutti70@gmail.com>
2020-02-26 22:01:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -50,20 +50,16 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-
+  num = gets.chomp
+  index = input_to_index
+  if valid_move?(board, index)
+    display_board(board)
+  else
+    turn(board)
 end
 
-# Asking the user for their move by position 1-9.
-# Receiving the user input.
-# Convert position to an index.
+#  ask for input == Asking the user for their move by position 1-9.
+#  get input == Receiving the user input.
+# convert input to index == Convert position to an index.
 # If the move is valid, make the move and display the board to the user.
 # If the move is invalid, ask for a new move until a valid move is received.
-# ask for input
-# get input
-# convert input to index
-# if index is valid
-#   make the move for index
-#   show the board
-# else
-#   ask for input again until you get a valid input
-# end
