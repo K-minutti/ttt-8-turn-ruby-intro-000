d72cc00017e34fd02c4c2ce60d26bad3bec28181
Kevin Minutti <minutti70@gmail.com>
2020-02-26 22:18:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,4 @@
-#displays a tic tac toe board, with board spaces passed as an array
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,14 +7,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#coverts a user's place on the board to the index integer
+
 def input_to_index(user_input)
   index = user_input.to_i
   index -= 1
   return index
 end
 
-#checks the number to see if 1) the position is already taken and 2)it is an actual number
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
@@ -39,13 +38,12 @@ def valid_move?(board, index)
   end
 end
 
-#Takes a users num and the board, and places the X or O char in that position
+
 def move(board, index, character = "X")
   board[index] = character
   return board
 end
 
-#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
 def turn (board)
   puts "Please enter 1-9:"
   num = gets.chomp
@@ -57,64 +55,7 @@ def turn (board)
     turn(board)
   end
 end
-# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-#
-# def display_board(board)
-#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-#   puts "-----------"
-#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-#   puts "-----------"
-#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-# end
-#
-# def input_to_index(user_input)
-#   index = user_input.to_i
-#   index -= 1
-#   return index
-# end
-#
-# # validmove? methods
-# def on_board?(index_num)
-#   if  index_num.between?(0,8) == true
-#     return true
-#   else
-#     return false
-#   end
-# end
-#
-# def valid_move?(board, index)
-#   if (position_taken?(board, index)) == false && (on_board?(index) == true)
-#     return true
-#   else
-#     return false
-#   end
-# end
-#
-# #position_take method for use as helper method
-# def position_taken?(board, index)
-#  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-#   false
-#  else
-#   true
-#  end
-# end
-#
-# # move method
-# def move(board, index, character = "X")
-#   board[index] = character
-#   return board
-# end
-#
-# def turn(board)
-#   puts "Please enter 1-9:"
-#   num = gets.chomp
-#   index = input_to_index(num)
-#   if valid_move?(board, index) == true
-#     display_board(board)
-#   else
-#     turn(board)
-#   end
-# end
+
 
 #  ask for input == Asking the user for their move by position 1-9.
 #  get input == Receiving the user input.
